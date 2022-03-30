1b35b82b2c09b3a7f69eb0a1fc62996ef777bb6d
Matthew Peters <matthew@matthewpeters.ca>
2022-03-29 22:16:27 -0500

    Added category to product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_29_183327) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_031441) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -58,6 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_29_183327) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,4 +72,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_29_183327) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "categories"
 end
