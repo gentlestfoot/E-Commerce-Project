4feeea4344b8a7d8c6ed3912ea6c51bbd254d47d
Matthew Peters <matthew@matthewpeters.ca>
2022-03-31 10:33:12 -0500

    Added static page controller
    - Added about view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  root to: "products#index"
+
   resources :orders
   resources :products
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/:page" => "static#show"
 end
