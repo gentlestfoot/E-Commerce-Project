493b96ef761edbbcb25dd106663bd54577de6ceb
Matthew Peters <matthew@matthewpeters.ca>
2022-04-04 22:53:47 -0500

    Categories are seeded from a csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# if Rails.env.development?
-#   AdminUser.create!(email: "admin@example.com", password: "password",
-#                     password_confirmation: "password")
-# end
+require "csv"
 
 Product.destroy_all
+Category.destroy_all
+
+CSV.foreach(Rails.root.join("lib/categories.csv")) do |row|
+  Category.create(
+    name:        row[0],
+    description: row[1]
+  )
+end
 
 Category.all.each do |category|
   30.times do
