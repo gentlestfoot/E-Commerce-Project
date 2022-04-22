ba5455cac0a7b14b404847558d9a52f1a27ae96d
Matthew Peters <matthew@matthewpeters.ca>
2022-04-21 23:24:14 -0500

    Added about page link

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :products
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get "/about" => "static#show"
+  get "/:page" => "static#show"
   get "/contact" => "static#show"
   get "/categories/:id" => "categories#show"
 end
