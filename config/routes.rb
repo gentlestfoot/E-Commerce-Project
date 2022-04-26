c1d5a3e21c744b88dd6fc55d00c596ceec7378b0
Matthew Peters <matthew@matthewpeters.ca>
2022-04-26 15:19:17 -0500

    Removed redundant route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get "/products/search" => "products#search"
-  get "/about" => "static#show"
   get "/cart" => "cart#index"
   post "/cart" => "cart#add"
   get "/contact" => "static#show"
