1e67564f009915365d0321286acbdcf71ca92291
Matthew Peters <matthew@matthewpeters.ca>
2022-04-26 15:56:46 -0500

    Fixed route ordering so the search matches
    - No longer being overriden by resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
 
+  get "/products/search" => "products#search"
   resources :orders
   resources :products
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get "/products/search" => "products#search"
   get "/cart" => "cart#index"
   post "/cart" => "cart#add"
   get "/contact" => "static#show"
